8addc4360b6978066df8cd2874c3527631c63cf8
Andrea C <mirthis@gmail.com>
2022-08-10 11:31:03 +0100

    add front end build

--- build/app.js
+++ build/app.js
@@ -8,6 +8,7 @@ require("express-async-errors");
 const cors_1 = __importDefault(require("cors"));
 const passport_1 = __importDefault(require("passport"));
 const express_session_1 = __importDefault(require("express-session"));
+const path_1 = __importDefault(require("path"));
 const middleware_1 = __importDefault(require("./utils/middleware"));
 const db_1 = require("./utils/db");
 const models_1 = require("./models");
@@ -61,7 +62,8 @@ passport_1.default.deserializeUser(async (userId, done) => {
 if (process.env.NODE_ENV !== 'test') {
     app.use(middleware_1.default.morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 }
-app.use(express_1.default.static('build-fe'));
+const buildPath = path_1.default.join(__dirname, 'build-fe');
+app.use(express_1.default.static(buildPath));
 app.use('/api/users', controllers_1.usersRouter);
 app.use('/api/plaid', controllers_1.plaidRouter);
 app.use('/api/auth', controllers_1.authRouter);
@@ -69,6 +71,9 @@ app.use('/api/transactions', controllers_1.transactionsRouter);
 app.use('/api/accounts', controllers_1.accountsRouter);
 app.use('/api/categories', controllers_1.categoriesRouter);
 app.use('/api/demo', controllers_1.demoRouter);
+app.get('(/*)?', async (_req, res) => {
+    res.sendFile(`${buildPath}/index.html`);
+});
 app.use(middleware_1.default.errorHandler);
 app.use(middleware_1.default.unknownEndpoint);
 exports.default = app;
